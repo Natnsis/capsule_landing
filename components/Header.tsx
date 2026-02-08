0eb2a4031db8ed2837bd87dfedcf0846b0ac32e3
Nati <nsisay49@gmail.com>
2026-02-08 16:41:08 +0300

    cleaning globals.css

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const Header = () => {
+  return (
+    <header className="flex justify-center items-center">
+      <Link href="/notion">Notion</Link>
+      <Link href="/pick and drop">Notion</Link>
+      <Link href="/hover image">Notion</Link>
+    </header>
+  )
+}
+
+export default Header
